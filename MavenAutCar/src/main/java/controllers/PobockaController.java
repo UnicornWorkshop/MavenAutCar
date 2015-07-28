932ba65a7bbd23f115b3de104a744c4e81e6849e
Niellon <Dite.Viktor@gmail.com>
2015-07-28 13:08:25 +0200

    web.xml, .jsp soubory

--- MavenAutCar/src/main/java/controllers/PobockaController.java
+++ MavenAutCar/src/main/java/controllers/PobockaController.java
@@ -1,5 +1,17 @@
 package controllers;
 
-public class PobockaController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/pobocka")
+public class PobockaController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	   public String printHello(/*ModelMap model*/) {
+	      //model.addAttribute("message", "Hello Spring MVC Framework!");
+	      return "hello";
+	   }
 }
