68abc41c1a40b414177a43b7a83dec1b4c5b8d75
Niellon <Dite.Viktor@gmail.com>
2015-07-29 12:36:56 +0200

    vybavadao

---
+++ MavenAutCar/src/main/java/dao/VybavaDao.java
@@ -0,0 +1,64 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import entities.Rezervace;
+import entities.Vybava;
+
+public class VybavaDao extends AbstractDao implements GenericDao<Vybava> {
+
+	@Override
+	public void add(Vybava item) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		s.persist(item);
+		t.commit();
+		s.close();				
+	}
+
+	@Override
+	public List<Vybava> all() {
+		List<Vybava> vybava = new ArrayList<>();
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+
+		Query q = s.createQuery("SELECT v FROM Vybava v");
+		Iterator<Vybava> vysledek = q.iterate();
+		while (vysledek.hasNext()) {
+			vybava.add(vysledek.next());
+		}
+
+		t.commit();
+		s.close();
+		return vybava;
+	}
+
+	@Override
+	public Vybava update(Integer id, Vybava data) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Vybava temp = (Vybava) s.get(Vybava.class, id);
+		temp.setNazev(data.getNazev());
+		t.commit();
+		s.close();
+		return temp;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Query q = s.createQuery("DELETE Vybava v WHERE v.id=:id")
+				.setParameter("id", id);
+		q.executeUpdate();
+		t.commit();
+		s.close();	
+	}
+
+}
