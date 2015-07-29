9dee6d2882d14c8993b5f30160244c5c02db706d
Niellon <Dite.Viktor@gmail.com>
2015-07-29 10:04:24 +0200

    pobocka controller, dao

--- MavenAutCar/src/main/java/controllers/PobockaController.java
+++ MavenAutCar/src/main/java/controllers/PobockaController.java
@@ -4,14 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import dao.GenericDao;
+import dao.KlientDao;
+import dao.PobockaDao;
+import entities.Klient;
+import entities.Pobocka;
 
 @Controller
 @RequestMapping("/pobocka")
 public class PobockaController {
+		
+	@RequestMapping(value="/pobocky", method=RequestMethod.GET)
+	public ModelAndView clients() {
+		
+		GenericDao<Pobocka> dao = new PobockaDao();
+		
+		ModelAndView mav = new ModelAndView("pobocka/pobocky");
+		mav.addObject("pobocky", dao.showAll());
+
+		return mav;
+	}
 	
-	@RequestMapping(method = RequestMethod.GET)
-	   public String printHello(/*ModelMap model*/) {
-	      //model.addAttribute("message", "Hello Spring MVC Framework!");
-	      return "hello";
-	   }
 }
---
+++ MavenAutCar/src/main/java/dao/PobockaDao.java
@@ -0,0 +1,70 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import entities.Pobocka;
+
+public class PobockaDao extends AbstractDao implements GenericDao<Pobocka> {
+	
+	public void add(Pobocka p) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		s.persist(p);
+		t.commit();
+		s.close();
+
+		System.out.println("Pobocka: " + p.getMesto());
+	}
+
+	public List<Pobocka> showAll() {
+		List<Pobocka> pobocky = new ArrayList<>();
+
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+
+		Query q = s.createQuery("SELECT p FROM Pobocka p");
+		Iterator<Pobocka> vysledek = q.iterate();
+		while (vysledek.hasNext()) {
+			pobocky.add(vysledek.next());
+		}
+
+		t.commit();
+		s.close();
+		return pobocky;
+	}
+
+	public Pobocka showOne(Integer id) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Pobocka p = (Pobocka) s.get(Pobocka.class, id);
+		t.commit();
+		s.close();
+		return p;
+	}
+
+	public void update(Integer id, Pobocka zmeny) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Pobocka puvodni = (Pobocka) s.get(Pobocka.class, id);
+		puvodni.setMesto(zmeny.getMesto());
+		t.commit();
+		s.close();
+	}
+
+	public void delete(Integer id) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Query q = s.createQuery("DELETE Pobocka p WHERE p.id=:id")
+				.setParameter("id", id);
+		q.executeUpdate();
+		t.commit();
+		s.close();
+	}
+
+}
