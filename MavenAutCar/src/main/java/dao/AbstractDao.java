a50ba642913adcfedc6496946889242e42e93281
Niellon <Dite.Viktor@gmail.com>
2015-07-29 14:49:39 +0200

    simple test

--- MavenAutCar/src/main/java/dao/AbstractDao.java
+++ MavenAutCar/src/main/java/dao/AbstractDao.java
@@ -2,12 +2,17 @@ package dao;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 
 public abstract class AbstractDao {
 
-	private SessionFactory sf = new Configuration().configure()
-			.buildSessionFactory();
+	/*private SessionFactory sf = new Configuration().configure()
+			.buildSessionFactory();*/
+	@Autowired
+	private SessionFactory sf;
 
+	@Required
 	public void setSf(SessionFactory sf) {
 		this.sf = sf;
 	}
