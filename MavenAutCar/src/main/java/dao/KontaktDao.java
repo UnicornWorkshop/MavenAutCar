8c867e25730be12fb06ac49a6010e3d185c9642c
Pavel <savrovp@gmail.com>
2015-07-29 10:13:23 +0200

    final KontaktDao

---
+++ MavenAutCar/src/main/java/dao/KontaktDao.java
@@ -0,0 +1,79 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.persistence.EnumType;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import entities.Auto;
+import entities.Kontakt;
+
+public class KontaktDao extends AbstractDao implements GenericDao<Kontakt> {
+
+	@Override
+	public void add(Kontakt item) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		s.persist(item);
+		t.commit();
+		s.close();
+	}
+
+	@Override
+	public Kontakt showOne(Integer id) {
+		Kontakt k = new Kontakt();
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		k = (Kontakt) s.get(Kontakt.class, id);
+		t.commit();
+		s.close();
+		return k;
+	}
+
+	@Override
+	public List<Kontakt> showAll() {
+		List<Kontakt> contacts = new ArrayList<Kontakt>();
+
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+
+		Query q = s.createQuery("SELECT k FROM Kontakt k");
+		Iterator<Kontakt> result = q.iterate();
+		while (result.hasNext()) {
+			contacts.add(result.next());
+		}
+
+		t.commit();
+		s.close();
+		return contacts;
+	}
+
+	@Override
+	public void update(Integer id, Kontakt data) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Kontakt temp = (Kontakt) s.get(Kontakt.class, id);
+		temp.setKlient(data.getKlient());
+		temp.setTyp((data.getTypEnum()));
+		temp.setData(data.getData());
+		t.commit();
+		s.close();		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		Session s = getSf().openSession();
+		Transaction t = s.beginTransaction();
+		Query q = s.createQuery("DELETE Kontakt k WHERE k.id=:id")
+				.setParameter("id", id);
+		q.executeUpdate();
+		t.commit();
+		s.close();		
+	}
+
+}
