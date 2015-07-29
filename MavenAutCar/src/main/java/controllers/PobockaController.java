fa448df327f74b0aa7b32d1568a753427bf7e3a0
Pavel <savrovp@gmail.com>
2015-07-29 11:53:58 +0200

    blah

--- MavenAutCar/src/main/java/controllers/PobockaController.java
+++ MavenAutCar/src/main/java/controllers/PobockaController.java
@@ -1,52 +1,39 @@
 package controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import dao.GenericDao;
 import dao.PobockaDao;
 import entities.Pobocka;
 
-@Controller
-@RequestMapping("/pobocka")
-public class PobockaController {
-		
-	@RequestMapping(value="/all", method=RequestMethod.GET)
-	public ModelAndView clients() {
-		
-		GenericDao<Pobocka> dao = new PobockaDao();
-		
-		ModelAndView mav = new ModelAndView("pobocka/pobocky");
-		mav.addObject("pobocky", dao.showAll());
+public class PobockaController implements GenericController<Pobocka> {
 
+	@Override
+	public ModelAndView all() {
+		GenericDao<Pobocka> dao = new PobockaDao();
+		ModelAndView mav = new ModelAndView("office/all");
+		mav.addObject("offices", dao.all());
 		return mav;
 	}
-	
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public @ResponseBody Pobocka addPobocka(@RequestBody final Pobocka pobocka) {
-		
-		Pobocka p = new Pobocka(pobocka.getMesto());
+
+	@Override
+	public Pobocka add(Pobocka item) {
 		GenericDao<Pobocka> dao = new PobockaDao();
-		dao.add(p);
-		return p;
+		dao.add(item);
+		return item;
 	}
-	
-	@RequestMapping(value="/update/{id}", method=RequestMethod.PUT)
-	public @ResponseBody Pobocka updatePobocka(@RequestBody final Pobocka zmeny) {
-		/*System.out.println("id: " + klient.getId());
-		System.out.println("jmeno: " + klient.getJmeno());
-		klient.setJmeno("bc. " + klient.getJmeno());
-		
-		Klient k = new Klient(klient.getJmeno());
-		GenericDao<Klient> dao = new KlientDao();
-		dao.add(k);
-		
-		return klient;*/
-		return new Pobocka(); //pak smazat
+
+	@Override
+	public Pobocka update(Pobocka item, Integer id) {
+		GenericDao<Pobocka> dao = new PobockaDao();
+		dao.delete(id);
+		return item;
 	}
+
+	@Override
+	public void delete(Integer id) {
+		GenericDao<Pobocka> dao = new PobockaDao();
+		dao.delete(id);
+	}
+		
 }
