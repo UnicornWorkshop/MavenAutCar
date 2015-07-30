c0533a69539b23b8a21232e8b19cba72d31be4b0
Niellon <Dite.Viktor@gmail.com>
2015-07-30 10:28:27 +0200

    session

--- MavenAutCar/src/main/java/dao/AbstractDao.java
+++ MavenAutCar/src/main/java/dao/AbstractDao.java
@@ -1,15 +1,29 @@
 package dao;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractDao {
 	
 	@Autowired
-	Session session;
+	private SessionFactory sf;
+	private Session session;
 
+	public SessionFactory getSf() {
+		return sf;
+	}
+		
+	public void createSession() {
+		session = sf.openSession();
+	}
+	
 	public Session getSession() {
 		return session;
+	}
+	
+	public void closeSession() {
+		session.close();
 	}
 		
 }
