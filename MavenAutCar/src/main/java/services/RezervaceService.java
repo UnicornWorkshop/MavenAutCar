694f3ca984213bd6041c0bd9e2d1ba6bf7ba1367
Niellon <Dite.Viktor@gmail.com>
2015-07-30 15:35:27 +0200

    oprava add

--- MavenAutCar/src/main/java/services/RezervaceService.java
+++ MavenAutCar/src/main/java/services/RezervaceService.java
@@ -2,15 +2,29 @@ package services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import dao.GenericDao;
 import entities.Rezervace;
+import entities.Pobocka;
+import entities.Klient;
 
 @Component
 public class RezervaceService extends AbstractService<Rezervace> implements GenericService<Rezervace> {
 	
+	@Autowired
+	GenericDao<Klient> klientDao;
+	@Autowired
+	GenericDao<Pobocka> pobockaDao;
+	
 	@Override
 	public void add(Rezervace item) {
+		//vytahnout klienta a pobocky a ulozit to nejdriv
+		klientDao.insert(item.getKlient());
+		pobockaDao.insert(item.getPobocka_z());
+		pobockaDao.insert(item.getPobocka_do());		
+		
 		getDao().insert(item);
 	}
 
