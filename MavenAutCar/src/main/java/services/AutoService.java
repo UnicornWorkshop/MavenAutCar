b3f0adf939b67faab511bea6d891fd6fd130a941
Niellon <Dite.Viktor@gmail.com>
2015-07-30 11:56:03 +0200

    auto service bez update

--- MavenAutCar/src/main/java/services/AutoService.java
+++ MavenAutCar/src/main/java/services/AutoService.java
@@ -13,33 +13,23 @@ public class AutoService extends AbstractService<Auto> implements GenericService
 	public void add(Auto item) {
 		getDao().insert(item);
 	}
-	/*@Override
-	public void add(Auto item) {
-		Session s = getSf().openSession();
-		Transaction t = s.beginTransaction();
-		s.persist(item);
-		t.commit();
-		s.close();
-	}
 
 	@Override
 	public List<Auto> all() {
-		List<Auto> cars = new ArrayList<Auto>();
-
-		Session s = getSf().openSession();
-		Transaction t = s.beginTransaction();
-
-		Query q = s.createQuery("SELECT a FROM Auto a");
-		Iterator<Auto> result = q.iterate();
-		while (result.hasNext()) {
-			cars.add(result.next());
-		}
-
-		t.commit();
-		s.close();
-		return cars;
+		return getDao().getAll();
 	}
 
+	@Override
+	public Auto update(Integer id, Auto data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		getDao().delete(id);
+	}
+	/*
 	@Override
 	public Auto update(Integer id, Auto data) {
 		Session s = getSf().openSession();
@@ -54,34 +44,6 @@ public class AutoService extends AbstractService<Auto> implements GenericService
 		t.commit();
 		s.close();
 		return temp;
-	}
-
-	@Override
-	public void delete(Integer id) {
-		Session s = getSf().openSession();
-		Transaction t = s.beginTransaction();
-		Query q = s.createQuery("DELETE Auto a WHERE a.id=:id").setParameter(
-				"id", id);
-		q.executeUpdate();
-		t.commit();
-		s.close();
 	}*/
 
-	@Override
-	public List<Auto> all() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Auto update(Integer id, Auto data) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void delete(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
 }
